79c170f9435c9dd3c85e04ed072d3b920e6c1245
Basil Suter <git@nadar.io>
2023-02-08 10:00:43 +0000

    init

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 openapi-generator-cli generate -i  https://api.flyo.cloud/nitro/openapi \
     -g javascript \
+    -o . \
     --additional-properties=usePromises=true,projectName=nitrocms
